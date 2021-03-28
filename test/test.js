0003b11a9f4f18db19ef35d8278c990e421287fa
adenicole <dedoyinnicole@gmail.com>
2021-03-28 13:19:35 +0000

    add deployment script

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
  it('respond with hey Nicole', function(done) {
- //navigate to root and check the the response is "hello world"
+ //navigate to root and check the the response is "hey Nicole"
  request(app).get('/').expect('hey Nicole', done);
  });
 });
